84031f29c227b8662064921b68feb5a48c4ffe2a
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-01-13 17:53:57 +0530

    13th Jan

---
+++ 1_first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    printf("Hey there!, I am Ashmit \n");
+    system("pause");
+    return 0;
+}
+// Use -o to get the desired file name from gcc
---
+++ 2_variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a = 5; //This is an integer
+    float b = 5.6; // This is a decimal real no.
+    char c = 'halua'; //This is a character
+    printf("The value of a is %d \n",a); //%d is used for integer val
+    printf("The value of b is %f \n",b); //%f is used for decimal val (float)
+    printf("The value of c is %c \n",c); //%c is used for characters
+    printf("The value of a+b is %f \n",a+b); //%c is used for characters
+    system("pause");
+    return 0;
+}
